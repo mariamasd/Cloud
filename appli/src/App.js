4e795219d0204e0b1b2774a4ab4ee6283a727ff5
mariamasd <mariamasadou2307@gmail.com>
2023-03-23 10:46:10 +0000

    ajout

---
+++ appli/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> BIENVENUE A GOMYCODE</h1>
+     <iframe width="520" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" id="gmap_canvas" src="https://maps.google.com/maps?width=520&amp;height=400&amp;hl=en&amp;q=%20gomycode+(chez%20moi)&amp;t=&amp;z=13&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
+
+    </div>
+  );
+}
+
+export default App;
